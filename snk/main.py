8efcccb3747dd24da68e0bc2be02b51c4a98f60e
Wytamma Wirth <wytamma.wirth@me.com>
2023-01-18 14:31:15 +1100

    :art: move home and bin to global config

--- snk/main.py
+++ snk/main.py
@@ -7,9 +7,28 @@ from .nest import Nest
 
 app = typer.Typer()
 
+SNK_HOME = None
+SNK_BIN = None
 
 @app.callback(context_settings={"help_option_names": ["-h", "--help"]})
-def callback():
+def callback(
+    home: Optional[Path] = typer.Option(
+            None, 
+            envvar="SNK_HOME", 
+            dir_okay=True, 
+            file_okay=False, 
+            exists=True, 
+            help="Overrides default snk location. Pipelines will be installed in $SNK_HOME/pipelines."
+        ),
+    bin: Optional[Path] = typer.Option(
+            None, 
+            envvar="SNK_BIN", 
+            dir_okay=True, 
+            file_okay=False, 
+            exists=True, 
+            help="Overrides location of pipeline installations. Pipelines are symlinked here."
+        )
+    ):
     """\b
         _            _             _        
        / /\         /\ \     _    /\_\      
@@ -26,41 +45,47 @@ def callback():
  \n
  Snakemake pipeline management system
     """
+    global SNK_BIN, SNK_HOME
+    SNK_BIN = bin
+    SNK_HOME = home
+    
 
 
 @app.command()
 def install(
-        pipeline: str = typer.Argument(..., help="URL or Github name (user/repo) of the pipeline to install."),
-        name: Optional[str] = typer.Option(None, help="Rename the pipeline (this name will be used to call the CLI.)"),
-        home: Optional[Path] = typer.Option(None, envvar="SNK_HOME", dir_okay=True, file_okay=False, exists=True, help="Overrides default snk location. Path to directory where all pipelines will be installed."),
-        bin: Optional[Path] = typer.Option(None, envvar="SNK_BIN", dir_okay=True, file_okay=False, exists=True, help="Overrides location of pipeline installations. Path to folder in PATH where pipelines are symlinked."),
+        pipeline: str = typer.Argument(
+            ..., help="URL or Github name (user/repo) of the pipeline to install."
+        ),
+        name: Optional[str] = typer.Option(
+            None, 
+            help="Rename the pipeline (this name will be used to call the CLI.)"
+        ),
     ):
     """
     Install a pipeline.
     """
-    nest = Nest(snk_home=home, bin_dir=bin)
+    
+    nest = Nest(snk_home=SNK_HOME, bin_dir=SNK_BIN)
     if not pipeline.startswith('http'):
         pipeline = f"https://github.com/{pipeline}.git"
         typer.echo(f'Installing Pipeline from Github: {pipeline}')
     cli = nest.install(repo_url=pipeline, name=name)
     typer.secho(f"Successfully installed {cli.name}!")
 
+
 @app.command()
 def uninstall(
         name: str = typer.Argument(..., help="Name of the pipeline to uninstall."),
         force: Optional[bool] = typer.Option(False, '--force', '-f', help="Force uninstall without asking."),
-        home: Optional[Path] = typer.Option(None,  envvar="SNK_HOME", dir_okay=True, file_okay=False, exists=True, help="Overrides default snk location. Path to directory where pipeline is installed."),
-        bin: Optional[Path] = typer.Option(None, envvar="SNK_BIN", dir_okay=True, file_okay=False, exists=True, help="Overrides location of pipeline installations. Path to folder in PATH where pipelines are symlinked."),
     ):
     """
     Uninstall a pipeline.
     """
-    nest = Nest(snk_home=home, bin_dir=bin)
+    nest = Nest(snk_home=SNK_HOME, bin_dir=SNK_BIN)
     uninstalled = nest.uninstall(name, force=force)
     if uninstalled:
         typer.secho(f"Successfully uninstalled {name}!")
         
-
 
 @app.command()
 def update():
