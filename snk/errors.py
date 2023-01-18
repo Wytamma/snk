aedfc18dd60257b5f5e760d6d381b3ca1e120dcd
Wytamma Wirth <wytamma.wirth@me.com>
2023-01-18 13:09:32 +1100

    :art: add error classes

---
+++ snk/errors.py
@@ -0,0 +1,11 @@
+class NestError(Exception):
+    """Base class for all pipeline exceptions"""
+
+class PipelineExistsError(NestError):
+    """Thrown if the given pipeline is already installed"""
+
+class PipelineNotFoundError(NestError):
+    """Thrown if the given pipeline cannot be found"""
+
+class InvalidPipelineRepositoryError(NestError):
+    """Thrown if the given repository appears to have an invalid format."""
