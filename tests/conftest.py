bc07e38677c7355256c0d6daee1a7621ade04d9a
Wytamma Wirth <wytamma.wirth@me.com>
2023-03-02 17:36:49 +1100

    :art: add clirunner

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
-import tempfile
 from pathlib import Path
 from snk import Nest
+from .utils import CLIRunner
 
 @pytest.fixture()
 def bin_dir(tmpdir: Path):
@@ -14,3 +14,16 @@ def snk_home(tmpdir: Path):
 @pytest.fixture()
 def nest(snk_home, bin_dir):
     return Nest(snk_home, bin_dir)
+
+@pytest.fixture()
+def example_config():
+    return Path("tests/data/config.yaml")
+
+@pytest.fixture()
+def deseq2_runner(nest: Nest):
+    deseq2 = nest.install('https://github.com/snakemake-workflows/rna-seq-star-deseq2.git')
+    expected = nest.pipelines_dir / 'rna-seq-star-deseq2'
+    assert expected.exists()
+    print(deseq2.executable)
+    runner = CLIRunner([deseq2.executable])
+    return runner
