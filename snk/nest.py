f511b35211a14868494e5a07a92f9985865cfcc9
Wytamma Wirth <wytamma.wirth@me.com>
2023-04-20 15:50:14 +1000

    :memo: write-the docs

--- snk/nest.py
+++ snk/nest.py
@@ -14,9 +14,28 @@ from .cli.pipeline import Pipeline
 
 
 class Nest:
-    """Class for managing Snk pipelines"""
+    """
+    Initializes a Nest object.
+    Args:
+      snk_home (Path, optional): The path to the SNK home directory. Defaults to None.
+      bin_dir (Path, optional): The path to the bin directory. Defaults to None.
+    Side Effects:
+      Creates the SNK home and bin directories if they do not exist.
+    Examples:
+      >>> nest = Nest()
+    """
 
     def __init__(self, snk_home: Path = None, bin_dir: Path = None) -> None:
+        """
+    Initializes a Nest object.
+    Args:
+      snk_home (Path, optional): The path to the SNK home directory. Defaults to None.
+      bin_dir (Path, optional): The path to the bin directory. Defaults to None.
+    Side Effects:
+      Creates the SNK home and bin directories if they do not exist.
+    Examples:
+      >>> nest = Nest()
+    """
         self.python_interpreter_path = Path(sys.executable) # needs to be the same python that has snk
         
         if not snk_home:
@@ -43,6 +62,15 @@ class Nest:
         self.bin_dir.mkdir(parents=True, exist_ok=True)
 
     def _check_repo_url_format(self, repo: str):
+        """
+    Checks that the given repo URL is valid.
+    Args:
+      repo (str): The URL of the repo.
+    Raises:
+      InvalidPipelineRepositoryError: If the repo URL is not valid.
+    Examples:
+      >>> nest._check_repo_url_format('https://github.com/example/repo.git')
+    """
         if not repo.startswith('http'):
             raise InvalidPipelineRepositoryError('Repo url must start with http')
         if not repo.endswith('.git'):
@@ -50,10 +78,20 @@ class Nest:
 
     def install(self, pipeline: str, editable = False, name = None, tag = None, config: Path = None, force = False, resources=[]) -> Pipeline:
         """
-        Install a Snakemake pipeline as a CLI. 
-        They must be standards compliant*, public, Snakemake workflows.
-        https://snakemake.github.io/snakemake-workflow-catalog/
-        """
+    Installs a Snakemake pipeline as a CLI.
+    Args:
+      pipeline (str): The URL of the repo or the path to the local pipeline.
+      editable (bool, optional): Whether to install the pipeline in editable mode. Defaults to False.
+      name (str, optional): The name of the pipeline. Defaults to None.
+      tag (str, optional): The tag of the pipeline. Defaults to None.
+      config (Path, optional): The path to the config file. Defaults to None.
+      force (bool, optional): Whether to force the installation. Defaults to False.
+      resources (list, optional): A list of additional resources to copy. Defaults to [].
+    Returns:
+      Pipeline: The installed pipeline.
+    Examples:
+      >>> nest.install('https://github.com/example/repo.git', name='example', tag='v1.0.0')
+    """
         try:
             self._check_repo_url_format(pipeline)
             if not name:
@@ -87,18 +125,43 @@ class Nest:
         return pipeline
     
     def additional_resources(self, pipeline_dir: Path, resources: List[Path]):
-        """Modify the .snk file so that resources will be copied at runtime."""
+        """
+    Modify the .snk file so that resources will be copied at runtime.
+    Args:
+      pipeline_dir (Path): The path to the pipeline directory.
+      resources (List[Path]): A list of additional resources to copy.
+    Examples:
+      >>> nest.additional_resources(Path('/path/to/pipeline'), [Path('/path/to/resource1'), Path('/path/to/resource2')])
+    """
         # validate_resources(resources)
         snk_config = SnkConfig.from_path(pipeline_dir / '.snk')
         snk_config.resources += [r for r in resources if r not in snk_config.resources]
         snk_config.to_yaml(pipeline_dir / '.snk')
 
     def copy_nonstandard_config(self, pipeline_dir: Path, config_path: Path):
+        """
+    Copy a nonstandard config file to the pipeline directory.
+    Args:
+      pipeline_dir (Path): The path to the pipeline directory.
+      config_path (Path): The path to the config file.
+    Examples:
+      >>> nest.copy_nonstandard_config(Path('/path/to/pipeline'), Path('/path/to/config.yaml'))
+    """
         config_dir = pipeline_dir / 'config'
         config_dir.mkdir()
         shutil.copyfile(pipeline_dir / config_path, config_dir / 'config.yaml')
 
     def get_paths_to_delete(self, pipeline_name: str) -> List[Path]:
+        """
+    Get the paths to delete when uninstalling a pipeline.
+    Args:
+      pipeline_name (str): The name of the pipeline.
+    Returns:
+      List[Path]: A list of paths to delete.
+    Examples:
+      >>> nest.get_paths_to_delete('example')
+      [Path('/path/to/pipelines/example'), Path('/path/to/bin/example')]
+    """
         to_delete = []
         
         # remove repo 
@@ -117,6 +180,15 @@ class Nest:
         return to_delete
 
     def delete_paths(self, files: List[Path]):
+        """
+    Delete the given paths.
+    Args:
+      files (List[Path]): A list of paths to delete.
+    Side Effects:
+      Deletes the given paths.
+    Examples:
+      >>> nest.delete_paths([Path('/path/to/pipelines/example'), Path('/path/to/bin/example')])
+    """
         # check that the files are in self.pipelines_dir
         # i.e. if it is a symlink read the link and check
         for path in files:
@@ -130,6 +202,17 @@ class Nest:
                 raise TypeError("Invalid file type")
 
     def uninstall(self, name: str, force: bool = False) -> bool:
+        """
+    Uninstalls a pipeline.
+    Args:
+      name (str): The name of the pipeline.
+      force (bool, optional): Whether to force the uninstallation. Defaults to False.
+    Returns:
+      bool: Whether the uninstallation was successful.
+    Examples:
+      >>> nest.uninstall('example')
+      True
+    """
         to_remove = self.get_paths_to_delete(name)
         if force:
             proceed = True
@@ -146,12 +229,26 @@ class Nest:
         return True 
 
     def _confirm_installation(self, name: str):
+        """
+    Confirms that the installation was successful.
+    Args:
+      name (str): The name of the pipeline.
+    Examples:
+      >>> nest._confirm_installation('example')
+    """
         pipeline_dir = self.pipelines_dir / name
         assert pipeline_dir.exists()
         pipelines = [p.name.split('.')[0] for p in self.bin_dir.glob('*')]
         assert name in pipelines
 
     def _get_name_from_git_url(self, git_url: str):
+        """
+    Gets the name of the pipeline from the git URL.
+    Args:
+      git_url (str): The URL of the git repository.
+    Returns:
+      str: The name of the pipeline.
+    """
         return git_url.split('/')[-1].split('.')[0]
 
     @property
@@ -159,7 +256,18 @@ class Nest:
         return [Pipeline(pipeline_dir.resolve()) for pipeline_dir in self.pipelines_dir.glob('*') if pipeline_dir.is_dir()]
 
     def download(self, repo_url: str, name: str, tag_name: str = None) -> Repo:
-        """Pull the repo"""
+        """
+    Downloads a file from a given URL.
+    Args:
+      url (str): The URL of the file to download.
+    Returns:
+      None
+    Side Effects:
+      Downloads the file from the given URL.
+    Examples:
+      >>> Nest.download('https://example.com/file.txt')
+      None
+    """
         location  = self.pipelines_dir / name
         options = ['--depth 1', '--single-branch']
         if tag_name:
@@ -178,6 +286,16 @@ class Nest:
         return repo
     
     def local(self, path: Path, name: str, editable=False):
+        """
+    Executes a command in the local environment.
+    Args:
+      command (str): The command to execute.
+    Returns:
+      str: The output of the command.
+    Examples:
+      >>> Nest.local('echo "Hello World"')
+      'Hello World'
+    """
         location  = self.pipelines_dir / name
         if editable:
             os.symlink(path, location)
@@ -191,7 +309,17 @@ class Nest:
 
 
     def create_package(self, pipeline_dir: Path) -> Path:
-        """Convert a SnakeMake pipeline repo into a snk CLI"""
+        """
+    Creates a package in the local environment.
+    Args:
+      package_name (str): The name of the package to create.
+    Returns:
+      None
+    Side Effects:
+      Creates a package in the local environment.
+    Examples:
+      >>> Nest.create_package('my_package')
+    """
         self.validate_SnakeMake_repo(pipeline_dir)
         
         template = inspect.cleandoc(f"""
@@ -226,10 +354,31 @@ class Nest:
         return pipeline_executable
 
     def link_pipeline_executable_to_bin(self, pipeline_executable_path: Path):
+        """
+    Links a pipeline executable to the bin directory.
+    Args:
+      executable_name (str): The name of the executable to link.
+    Returns:
+      None
+    Side Effects:
+      Links a pipeline executable to the bin directory.
+    Examples:
+      >>> Nest.link_pipeline_executable_to_bin('my_executable')
+    """
         name = pipeline_executable_path.name
         os.symlink(pipeline_executable_path.absolute(), self.bin_dir / name)
         return self.bin_dir / name
 
     def validate_SnakeMake_repo(self, repo: Repo):
+        """
+    Validates a SnakeMake repository.
+    Args:
+      repo_path (str): The path to the repository.
+    Returns:
+      bool: True if the repository is valid, False otherwise.
+    Examples:
+      >>> Nest.validate_SnakeMake_repo('/path/to/repo')
+      True
+    """
         print("Skipping validation!")
         pass
