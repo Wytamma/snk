4fb951dc8670ef8a7d18b700482781f55d27d93d
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-09-25 22:52:54 +1000

    :zap: adding script to build paper pfd

---
+++ paper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Instructions here: https://joss.readthedocs.io/en/latest/submitting.html#docker
+docker run --rm \
+    --volume $PWD:/data \
+    --user $(id -u):$(id -g) \
+    --env JOURNAL=joss \
+    openjournals/paperdraft
